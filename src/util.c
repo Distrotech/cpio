b28e426f03788dd4610c78e76ccf72891b15bfc5
Sergey Poznyakoff <gray@gnu.org.ua>
2005-01-31 12:53:09 +0000

    (open_archive): Use MODE_RW.

--- src/util.c
+++ src/util.c
@@ -801,14 +801,14 @@ open_archive (char *file)
   copy_in = process_copy_in;
 
   if (copy_function == copy_in)
-    fd = rmtopen (file, O_RDONLY | O_BINARY, 0666, rsh_command_option);
+    fd = rmtopen (file, O_RDONLY | O_BINARY, MODE_RW, rsh_command_option);
   else
     {
       if (!append_flag)
-	fd = rmtopen (file, O_WRONLY | O_CREAT | O_TRUNC | O_BINARY, 0666,
+	fd = rmtopen (file, O_WRONLY | O_CREAT | O_TRUNC | O_BINARY, MODE_RW,
 			rsh_command_option);
       else
-	fd = rmtopen (file, O_RDWR | O_BINARY, 0666, rsh_command_option);
+	fd = rmtopen (file, O_RDWR | O_BINARY, MODE_RW, rsh_command_option);
     }
 
   return fd;
