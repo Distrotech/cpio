a988ee39c0166e926848e7da9958ddaa9319825b
Greg Nietsky <gregory@distrotech.co.za>
2014-08-20 14:48:34 +0200

    Clean build and distribution

---
+++ intl/export.h
@@ -0,0 +1,8 @@
+/* -*- buffer-read-only: t -*- vi: set ro: */
+/* DO NOT EDIT! GENERATED AUTOMATICALLY! */
+
+#if @HAVE_VISIBILITY@ && BUILDING_LIBINTL
+#define LIBINTL_DLL_EXPORTED __attribute__((__visibility__("default")))
+#else
+#define LIBINTL_DLL_EXPORTED
+#endif
---
+++ m4/printf-posix.m4
@@ -0,0 +1,1 @@
+../gnulib/m4/printf-posix.m4
